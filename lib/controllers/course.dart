4b518f0278d6ceb10e3ab01e4d0b90ec48dfe7e3
Rachel <thelastsight@mail.ru>
2023-04-02 01:19:29 +1000

    colorScheme

---
+++ lib/controllers/course.dart
@@ -0,0 +1,11 @@
+import 'package:get/get.dart';
+
+import '../models/courses/course.dart';
+
+class CourseController extends GetxController {
+  String? title;
+
+  int? expAmount;
+
+  List<int>? partIds;
+}
